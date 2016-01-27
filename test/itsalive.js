37941da24636ea0a45ef2fd146fab8b3d9f69794
sp6pe <sp6pe@virginia.edu>
2016-01-27 12:06:41 -0500

    through virtuals

--- test/itsalive.js
+++ test/itsalive.js
@@ -1,56 +1,56 @@
-console.log("Frankenstein");
+// console.log("Frankenstein");
 
-var expect = require('chai').expect;
-var chai = require('chai');
-var spies = require('chai-spies');
-chai.use(spies);
+// var expect = require('chai').expect;
+// var chai = require('chai');
+// var spies = require('chai-spies');
+// chai.use(spies);
 
-describe("first test", function() {
-	var var1 = 0;
-	var var2 = 0;
-	beforeEach(function(){
-		var1 = 2;
-		var2 = 2;
-	});
-	it("equals 4", function() {
-		var result = var1 + var2;
-		expect(result).to.equal(4);
-	});
-});
+// describe("first test", function() {
+// 	var var1 = 0;
+// 	var var2 = 0;
+// 	beforeEach(function(){
+// 		var1 = 2;
+// 		var2 = 2;
+// 	});
+// 	xit("equals 4", function() {
+// 		var result = var1 + var2;
+// 		expect(result).to.equal(4);
+// 	});
+// });
 
 
-describe("how to use time difference", function() {
-	it('confirms setTimeout\'s timer accuracy', function (done) {
-	    var start = new Date();
-	    setTimeout(function () {
-	        var duration = new Date() - start;
-	        expect(duration).to.be.closeTo(1000, 50);
-	        done();
-	    }, 1000);
-	});
-})
+// describe("how to use time difference", function() {
+// 	xit('confirms setTimeout\'s timer accuracy', function (done) {
+// 	    var start = new Date();
+// 	    setTimeout(function () {
+// 	        var duration = new Date() - start;
+// 	        expect(duration).to.be.closeTo(1000, 50);
+// 	        done();
+// 	    }, 1000);
+// 	});
+// })
 
-describe("how to use spy", function() {
+// describe("how to use spy", function() {
 	
-	it('will invoke a function once per element', function () {
-	    var arr = ['x','y','z'];
-	    function logNth (val, idx) {
-	        console.log('Logging elem #'+idx+':', val);
-	    }
-	    logNth = chai.spy(logNth);
-	    arr.forEach(logNth);
-	    expect(logNth).to.have.been.called.exactly(arr.length);
-	});
-})
+// 	xit('will invoke a function once per element', function () {
+// 	    var arr = ['x','y','z'];
+// 	    function logNth (val, idx) {
+// 	        console.log('Logging elem #'+idx+':', val);
+// 	    }
+// 	    logNth = chai.spy(logNth);
+// 	    arr.forEach(logNth);
+// 	    expect(logNth).to.have.been.called.exactly(arr.length);
+// 	});
+// })
 
-describe('A test category', function () {
-    describe('A subcategory', function () {
-        xit('tests something', function () {});
-        xit('tests another aspect of the same thing' , function () {});
-    });
-    describe('A different subcategory', function () {});
-});
-describe('A different category', function () {});
+// describe('A test category', function () {
+//     describe('A subcategory', function () {
+//         xit('tests something', function () {});
+//         xit('tests another aspect of the same thing' , function () {});
+//     });
+//     describe('A different subcategory', function () {});
+// });
+// describe('A different category', function () {});
 
 
 
--- test/page.spec.js
+++ test/page.spec.js
@@ -14,11 +14,17 @@ chai.use(spies);
 
 
 describe('Test the Page Model', function () {
+    var page;
+  
+
+
     describe('See if validation works on page', function () {
-    	var page;
-    	beforeEach(function() {
-    		page = new Page()
-    	})
+        beforeEach(function() {
+            page = new Page()
+        })
+         afterEach(function(done){
+            Page.remove({}).then(function(){done();})
+        });
 
         it('Throws error if no title in POST request', function (done) {
         	//if pageSchema's title does not have a requirement of "true", it does 
@@ -28,22 +34,127 @@ describe('Test the Page Model', function () {
         		done()
         	})
         });
-        //xit('Throws error if no content in POST request' , function () {});
     });
+
     describe('See if static works', function () {
-    	beforeEach(function(done) {
-    		Page.create({
-    			title: 'foo',
-    			content: "bar",
-    			tags: ['foo','bar']
-    		}, done)
-    	})
-    	it('Throws error if tag is not in tags' , function () {
-    		console.log(Page.findByTag("foo"));
-    		Page.findByTag("foo").then(function(data) {
-    			console.log(data.tag);
-    		})
-    	});
+        beforeEach(function(done) {
+            Page.create({
+                title: 'jai',
+                content: "frank",
+                tags: ['jai','frank']
+            }, done)
+        });
+
+        afterEach(function(done){
+            Page.remove({}).then(function(){done()})
+        });
+
+        it('Throws error if tag is not in tags' , function (done) {
+            //console.log(Page.findByTag("foo"));
+            Page.findByTag("frank").then(function(data) {
+                expect(data).to.have.lengthOf(1);
+                done();
+            }).then(null,done)
+        });
+
+        it('it does not return pages w/o tag' , function (done) {
+            //console.log(Page.findByTag("foo"));
+            Page.findByTag("testingsucks").then(function(data) {
+                expect(data).to.have.lengthOf(0);
+                done();
+            }).then(null,done)
+        });
+
     });
+        //xit('Throws error if no content in POST request' , function () {});
+
+
+
+    describe('See if FIndSimilarWorks', function () {
+        var temp;
+        beforeEach(function(done) {
+            Page.create({
+                title: 'james',
+                content: "frankies",
+                tags: ['james','frankie']
+            }).then(function(){
+                Page.create({
+                    title: 'frankies',
+                    content: "james",
+                    tags: []
+                })
+            }).then(function() {
+                return Page.create({
+                    title: "Fullstack",
+                    content: "is 13 weeks long",
+                    tags:['james']
+                })
+            }).then(function(page1){
+                temp = page1;
+                done();
+            });
+
+        })
+
+        afterEach(function(done){
+            Page.remove({}).then(function(){done()})
+        });
+
+
+        it('Should fail when no match is made' , function (done) {
+                temp.findSimilar().then(function(data) {
+                    expect(data).to.have.lengthOf(1);
+                    done();
+                }).then(null,done)
+        });
+    });
+    
+    describe('See if virtuals work', function () {
+        var temp;
+
+         beforeEach(function(done) {
+            Page.create({
+                title: 'james franklin is awesome',
+                content: "frankies",
+                tags: ['james','frankie']
+            }).then(function(page1){
+                temp = page1;
+                //console.log(temp);
+                done();
+            });
+
+        })
+
+         afterEach(function(done){
+            Page.remove({}).then(function(){done();})
+        });
+
+        it('It creates a urlTitle', function (done) {
+            expect(temp.route).to.equal('/wiki/james_franklin_is_awesome');
+                done();
+            });
+
+    });
+
+
+
+
 });
+
+
+
 describe('A different category', function () {});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
